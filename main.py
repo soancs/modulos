e4ef5a432bb8f6dca91c0af11a5001478ac7210b
Angelica Soto <angelica.soto1861@uppuebla.edu.mx>
2024-05-13 08:54:36 -0600

    Main y módulo creado

---
+++ main.py
@@ -0,0 +1,5 @@
+from mimodulo import Sensor
+
+if __name__=="__main__":
+    obj=Sensor()
+    print(onj.pin)
---
+++ mimodulo.py
@@ -0,0 +1,7 @@
+class Sensor:
+    def__init__(self):
+        self.pin=15
+
+    def inicializar(self,tipo):
+        self.tipo=tipo
+
